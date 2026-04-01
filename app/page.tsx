8419c0e7b15010333ac7c8d9bbd49dc3857ff757
wildlife-cinematic-pro <acharyabimalvet1@gmail.com>
2026-04-01 17:18:08 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -695,6 +695,7 @@ const [animalVibe, setAnimalVibe] = useState<AnimalVibe>("National Geographic Wi
 
       const watchTimeReport = buildWatchTimeReport("5-shot", 2);
       const motionStrength = arcMotionStrength[finalArc] ?? 70;
+    
    const soundDesignPack = buildSoundDesignPack(
   predator,
   prey,
